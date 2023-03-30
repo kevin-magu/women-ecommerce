53b9140052e230232e64131901e991ed3170b63b
kevin-magu <wanjaukevinmagu@gmail.com>
2023-03-31 02:37:36 +0300

    bug

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let cart = document.getElementById("cart-icon");
 let addtoCartbutton = document.getElementById("add-to-cart");
 let thankYou=document.getElementById("thankyou");
 let shoppingwithUs=document.getElementById("for-shooping-with-us");
-let checkoutmore = document.getElementById("check-out-more");
+let checkoutmore = document.getElementById("check-out");
 
 function moveCart(){
     cart.style.transform="translateX(130px)";
@@ -16,11 +16,13 @@ function moveCart(){
     shoppingwithUs.style.animation="messagemove 3s ease-in";
     shoppingwithUs.style.animationDelay="3s";
 
-    addtoCartbutton.style.width="0px";
-    addtoCartbutton.style.transition="4s ease-in-out"
-    addtoCartbutton.style.transitionDelay="3.2s"
 
-    checkoutmore.style.display="block";
+    addtoCartbutton.style.width="0px";
+    addtoCartbutton.style.transition="2s ease-in-out";
+    addtoCartbutton.style.transitionDelay="4.2s";
+
+    checkoutmore.style.display="flex";
+    checkoutmore.style.backgroundColor="red";
     /*checkoutmore.style.transform="translateY(70px)";
     checkoutmore.style.transition="2s ease-in";
     checkoutmore.style.transitionDelay="7s"; */
