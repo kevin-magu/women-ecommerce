d7532e773cebaea71800418ea292e53465f733db
kevin-magu <wanjaukevinmagu@gmail.com>
2023-04-05 01:30:49 +0300

    changed the first page design

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let cart = document.getElementById("cart-icon");
+let cart = document.getElementById('cart-icon');
 let addtoCartbutton = document.getElementById("add-to-cart");
 let thankYou=document.getElementById("thankyou");
 let shoppingwithUs=document.getElementById("for-shooping-with-us");
@@ -26,3 +26,34 @@ function moveCart(){
     checkoutmore.style.transition="2s ease-in";
     checkoutmore.style.transitionDelay="7s"; */
 }
+
+let advertvideo = document.querySelector('.advert-video');
+let video = advertvideo.querySelector('video');
+let text = advertvideo.querySelector('h3');
+
+//scrolling effect
+const controller = new ScrollMagic.Controller();
+const scene = new ScrollMagic.Scene({
+    duration: 55000,
+    triggerElement: advertvideo,
+    triggerHook: 0
+})  
+.setPin(advertvideo)
+.addIndicators()
+.addTo(controller);
+
+//video animation
+let accelamount = 0.723906;
+let scrollpos = 0;
+let delay =0;
+
+scene.on("update", e => {
+    scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(()=>{
+    delay += (scrollpos - delay) * accelamount;
+
+    video.currentTime = delay;
+}, 4.166666666666667);
+
